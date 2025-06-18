94f9384e9c35cc2d4a6e079a8617f801950f5088
AHHYUNJU <wndkgus0225@naver.com>
2025-06-18 10:29:25 +0900

    fix: 불필요한 option 제거

--- src/mocks/CartItem/cartItemHandler.ts
+++ src/mocks/CartItem/cartItemHandler.ts
@@ -5,15 +5,9 @@ export const cartItemHandler = [
   http.get("/cart-items", ({ request }) => {
     const url = new URL(request.url);
     const idOption = url.searchParams.get("id");
-    const quantityOption = url.searchParams.get("quantity");
     let items = CART_ITEM_MOCK_DATA;
     if (idOption !== null) {
       items = items.filter(({ id }) => id === Number(idOption));
-    }
-    if (quantityOption !== null) {
-      items = items.filter(
-        ({ cartQuantity }) => cartQuantity === Number(quantityOption)
-      );
     }
     return HttpResponse.json({
       ...CART_ITEM_MOCK_DATA,
