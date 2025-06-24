fa1d9e54f3cfb496184c76dc0a23c43b42428974
AHHYUNJU <wndkgus0225@naver.com>
2025-06-24 15:45:51 +0900

    feat: reset.css 파일 추가 및 버튼 스타일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles/reset.css";
 import { CartItemProvider } from "./CartItem/context/CartItemContext";
 import { CouponProvider } from "./Coupon/context/CouponContext";
 import { ShoppingCartPage } from "./pages/ShoppingCartPage/ShoppingCartPage";
