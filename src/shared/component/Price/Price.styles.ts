0652f20f866b910fc81232ad70418e97675c7146
AHHYUNJU <wndkgus0225@naver.com>
2025-06-25 00:06:03 +0900

    feat: Price 컴포넌트 및 스타일 추가

---
+++ src/shared/component/Price/Price.styles.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Name = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  color: #0a0d13;
+  padding: 12px;
+`;
+
+export const Price = styled.p`
+  font-size: 24px;
+  font-weight: 700;
+  color: #000000;
+  padding: 12px;
+`;
