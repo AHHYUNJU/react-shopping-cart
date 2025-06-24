910963da1cddea49e16e1c2e487aa610f277dd9c
AHHYUNJU <wndkgus0225@naver.com>
2025-06-25 01:38:33 +0900

    feat: 배송 정보 컴포넌트 및 스타일 추가

---
+++ src/shared/component/Shipping/Shipping.styles.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.p`
+  font-weight: 700;
+  font-size: 16px;
+  margin-bottom: 16px;
+  text-align: left;
+`;
+
+export const CheckWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const Option = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+`;
