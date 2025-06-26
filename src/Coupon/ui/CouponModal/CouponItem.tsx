e768aa6f52f26c8d635748f2747e64181ee9fa9e
AHHYUNJU <wndkgus0225@naver.com>
2025-06-26 23:50:53 +0900

    feat: CouponItem 컴포넌트에 체크박스 및 정보 포맷팅 기능 추가

--- src/Coupon/ui/CouponModal/CouponItem.tsx
+++ src/Coupon/ui/CouponModal/CouponItem.tsx
@@ -1,4 +1,10 @@
 import { CouponResponse } from "@/Coupon/types/CouponResponse";
+import { Checkbox } from "@/shared/component/Checkbox/Checkbox";
+import {
+  formatDate,
+  formatCurrency,
+  formatAvailableTime,
+} from "@/Coupon/utils/format";
 import * as S from "./CouponModal.styles";
 
 type CouponItemProps = {
@@ -8,11 +14,36 @@ type CouponItemProps = {
 };
 
 function CouponItem({ coupon, isChecked, onCheck }: CouponItemProps) {
+  const { description, expirationDate } = coupon;
   return (
     <S.CouponItem onClick={() => onCheck(coupon.id)}>
-      <input type="checkbox" checked={isChecked} readOnly />
-      <div>{coupon.description}</div>
-      <div>{coupon.expirationDate}</div>
+      <S.CouponToolbar>
+        <Checkbox type="checkbox" checked={isChecked} />
+        <S.Name>{description}</S.Name>
+      </S.CouponToolbar>
+      <S.CouponInfoWrapper>
+        <S.CouponInfo>만료일: {formatDate(expirationDate)}</S.CouponInfo>
+
+        {coupon.discountType === "fixed" && (
+          <>
+            <S.CouponInfo>
+              최소 주문 금액: {formatCurrency(coupon.minimumAmount)}
+            </S.CouponInfo>
+          </>
+        )}
+
+        {coupon.discountType === "freeShipping" && (
+          <S.CouponInfo>
+            최소 주문 금액: {formatCurrency(coupon.minimumAmount)}
+          </S.CouponInfo>
+        )}
+
+        {coupon.discountType === "percentage" && (
+          <S.CouponInfo>
+            사용 가능 기간: {formatAvailableTime(coupon.availableTime)}
+          </S.CouponInfo>
+        )}
+      </S.CouponInfoWrapper>
     </S.CouponItem>
   );
 }
